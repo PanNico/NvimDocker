25a66477ac1ba597be7fc4f3a71fe30c8ee83b53
PanNico <pancheri.nicola@gmail.com>
2026-07-21 11:42:53 +0100

    add dns

--- docker-run-nvim.sh
+++ docker-run-nvim.sh
@@ -7,6 +7,7 @@ echo "Configuring X11 access for Docker containers..."
 xhost +local:docker
 
 docker run -it --rm \
+  --dns 8.8.8.8 --dns 8.8.4.4 \
   -v "${HOME}:${HOME}" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e DISPLAY=$DISPLAY \
