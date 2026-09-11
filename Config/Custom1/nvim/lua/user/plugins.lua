11478b7dfc51fadc0178eed6bcad331896189a37
PanNico <pancheri.nicola@gmail.com>
2026-09-11 09:53:39 +0100

    improve configuration gitsigns

--- Config/Custom1/nvim/lua/user/plugins.lua
+++ Config/Custom1/nvim/lua/user/plugins.lua
@@ -174,6 +174,12 @@ return {
         numhl = false,
         linehl = false,
         word_diff = false,
+        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+        blame_formatter = nil, -- Use default
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil, -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
       })
     end
   },
